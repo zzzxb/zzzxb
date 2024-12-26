7e86359271296be57c00d235bd3a2af4f9fc2919
Zzzxb <idearfree@outlook.com>
2024-12-26 18:50:57 +0800

    天假top按钮

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
+var timer = null;
+
 $(document).ready(function() {
-  resetImgSize()
-})
+  resetImgSize();
+
+  $('.top').mousedown(function() {
+    var speed = 1;
+    timer = setInterval(function() {
+      speed++;
+      var scrollPosition = document.documentElement.scrollTop;
+      window.scrollTo(0, scrollPosition - speed);
+      if(scrollPosition === 0) clearInterval(timer);
+    }, 10);
+  }).mouseup(function() {
+    clearInterval(timer);
+  });
+});
 
 function resetImgSize() {
   var content = $(".content");
@@ -17,3 +31,20 @@ function resetImgSize() {
     }
   }
 }
+
+window.addEventListener('scroll', function() {
+  var scrollPosition = document.documentElement.scrollTop;
+  var windowHeight = window.innerHeight;
+  var tv = windowHeight * 0.3;
+  if(scrollPosition > tv) {
+    var op = (scrollPosition - tv) * 0.001;
+    $('.top').css({
+      'display': 'block',
+      'opacity': Math.min(op, 1)
+    });
+  }else {
+    $('.top').css({
+      'display': 'none',
+    });
+  }
+})
